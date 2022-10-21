e00097a7f56259beedadc69872aa102be19c2f61
Suleiman Yunus <suleiman.yunus@meltwater.org>
2022-10-21 20:25:51 +0000

    rename functions

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def evenNumbersForLoop():
+def printEvenNumbersForLoop():
   for number in range(1,31):
     if(number % 2 == 0):
       print(number)
 
-def evenNumbersWhileLoop():
+def printEvenNumbersWhileLoop():
   i = 1
   while(i < 31):
     if (i % 2 == 0):
