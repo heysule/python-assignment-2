3f172e9a84865613c957b6b4113bc5e5d611e02f
Suleiman Yunus <suleiman.yunus@meltwater.org>
2022-10-21 20:24:19 +0000

    Finish task two

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ def evenNumbersForLoop():
   for number in range(1,31):
     if(number % 2 == 0):
       print(number)
+
+def evenNumbersWhileLoop():
+  i = 1
+  while(i < 31):
+    if (i % 2 == 0):
+      print(i)
+    i += 1
