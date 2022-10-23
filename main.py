4624f63d6939b9514a3633c5142bb746eccd3bc0
Suleiman Yunus <suleiman.yunus@meltwater.org>
2022-10-23 14:18:35 +0000

    task 2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# With a for & while loop, print even numbers from 1 - 30.
 def printEvenNumbersForLoop():
   for number in range(1,31):
     if(number % 2 == 0):
@@ -7,5 +8,18 @@ def printEvenNumbersWhileLoop():
   i = 1
   while(i < 31):
     if (i % 2 == 0):
+      print(i)
+    i += 1
+
+# With a for & while loop, print odd numbers from 1 - 30.
+def printOldNumbersForLoop():
+  for number in range(1,31):
+    if(number % 2 != 0):
+      print(number)
+
+def printOldNumbersWhileLoop():
+  i = 1
+  while(i < 31):
+    if (i % 2 != 0):
       print(i)
     i += 1
